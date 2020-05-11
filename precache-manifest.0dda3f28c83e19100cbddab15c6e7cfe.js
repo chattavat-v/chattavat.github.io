e223fdfd624c372aa4606a9929b463801fed0fea
chattavat.macOs <chattavat.v@gmail.com>
2020-05-11 22:48:32 +0700

    rebuild

---
+++ precache-manifest.0dda3f28c83e19100cbddab15c6e7cfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996087aeaa3ecf23aff738a6b1c8ca2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7aaf036754a2155c8d7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4dde68c03fc12d478350",
+    "url": "/static/js/2.bf1ae51c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf1ae51c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7aaf036754a2155c8d7",
+    "url": "/static/js/main.4daef9e7.chunk.js"
+  },
+  {
+    "revision": "656aa50fbad87425b122",
+    "url": "/static/js/runtime-main.b1cc144f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

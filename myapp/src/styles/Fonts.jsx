ff5060de5ccd56d9fe2f4085e53f730b3c0a7f2a
chattavat.macOs <chattavat.v@gmail.com>
2020-05-16 18:32:23 +0700

    initial font styles

---
+++ myapp/src/styles/Fonts.jsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const blck = styled.p`
+	font-family: "Roboto-Black";
+`;
+
+export const blackItalic = styled.p`
+	font-family: "Roboto-BlackItalic";
+`;
+
+export const bold = styled.p`
+	font-family: "Roboto-Bold";
+`;
+
+export const boldItalic = styled.p`
+	font-family: "Roboto-BoldItalic";
+`;
+
+export const Italic = styled.p`
+	font-family: "Roboto-Italic";
+`;
+
+export const light = styled.p`
+	font-family: "Roboto-Light";
+`;
+
+export const lightItalic = styled.p`
+	font-family: "Roboto-LightItalic";
+`;
+
+export const medium = styled.p`
+	font-family: "Roboto-Medium";
+`;
+
+export const mediumItalic = styled.p`
+	font-family: "Roboto-MediumItalic";
+`;
+
+export const regular = styled.p`
+	font-family: "Roboto-Regular";
+`;
+
+export const thin = styled.p`
+	font-family: "Roboto-Thin";
+`;
+
+export const thinItalic = styled.p`
+	font-family: "Roboto-ThinItalic";
+`;

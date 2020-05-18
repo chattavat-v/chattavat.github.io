aeaec12026ba7adbff5883e8581524a47269716d
chattavat.macOs <chattavat.v@gmail.com>
2020-05-18 22:08:59 +0700

    build navbar component

---
+++ myapp/src/views/header-view/HeaderView.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState, useCallback } from "react";
+import PropTypes from "prop-types";
+import * as Header from "./HeaderStyle";
+import * as utils from "./utils";
+import { H2, P, Capitalize } from "../../styles/Fonts";
+
+const HeaderView = (props) => {
+	const [isScroller, setIsScroller] = useState(window.scrollY);
+	const [isWrapperHeader, setIsWrapperHeader] = useState(Header.InitialHeader);
+
+	const scrollHandler = useCallback(() => {
+		setIsScroller(window.scrollY);
+	}, []);
+
+	useEffect(() => {
+		window.addEventListener("scroll", scrollHandler);
+
+		return () => {
+			window.removeEventListener("scroll", scrollHandler);
+		};
+	}, [scrollHandler]);
+
+	useEffect(() => {
+		isScroller > 100
+			? setIsWrapperHeader(Header.ScrollerHeader)
+			: setIsWrapperHeader(Header.InitialHeader);
+	});
+
+	return (
+		<Header.WrapperHeader style={isWrapperHeader}>
+			<Header.InlineHeader>
+				<Header.WrapperLogo>
+					<H2 style={Capitalize}>{utils.FirstName}</H2>
+					<H2 style={Header.LastNameItem}>{utils.LastName}</H2>
+				</Header.WrapperLogo>
+				<Header.WrapperNavigation>
+					<Header.ListNavigation>
+						{utils.TopicLists.map((item) => (
+							<Header.ListItem key={item._id}>
+								<P style={Header.LabelItem}>{item.label}</P>
+							</Header.ListItem>
+						))}
+					</Header.ListNavigation>
+				</Header.WrapperNavigation>
+			</Header.InlineHeader>
+		</Header.WrapperHeader>
+	);
+};
+
+HeaderView.propTypes = {};
+
+export default HeaderView;
---
+++ myapp/src/views/header-view/utils.js
@@ -0,0 +1,12 @@
+export const FirstName = "chattavat.";
+export const LastName = "v";
+
+export const TopicLists = [
+	{ _id: "1", label: "home" },
+	{ _id: "2", label: "about" },
+	{ _id: "3", label: "training" },
+	{ _id: "4", label: "career" },
+	{ _id: "5", label: "project" },
+	{ _id: "6", label: "experience" },
+	{ _id: "7", label: "contact" },
+];

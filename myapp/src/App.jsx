956d0ed0e60edb88ab39831387895f151aa767ea
chattavat.macOs <chattavat.v@gmail.com>
2020-05-19 22:52:57 +0700

    fixed warning

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as Global from "./styles/Global";
-import * as fonts from "./styles/Fonts";
 import MainScreen from "./screens/main-screen/MainScreen";
 
 const App = () => {
--- myapp/src/screens/main-screen/MainScreen.jsx
+++ myapp/src/screens/main-screen/MainScreen.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import HeaderView from "../../views/header-view/HeaderView";
+import SideView from "../../views/side-view/SideView";
 
 const MainScreen = (props) => {
+	const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
+
 	return (
 		<>
-			<HeaderView />
-			<div style={{ height: "200vh" }}>Hello</div>
+			<HeaderView
+				drawerToggleClickHandler={() => setSideDrawerOpen(!sideDrawerOpen)}
+			/>
+			<SideView onShowSideDrawer={sideDrawerOpen} />
+			<div style={{ height: "200vh" }}></div>
 		</>
 	);
 };

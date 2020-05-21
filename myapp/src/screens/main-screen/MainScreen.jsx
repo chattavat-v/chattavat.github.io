50cfcbd4ab41246275ea423f0c1f3d87c12ba2c5
chattavat.macOs <chattavat.v@gmail.com>
2020-05-21 22:31:36 +0700

    build backdrop view and add condition when click background

--- myapp/src/screens/main-screen/MainScreen.jsx
+++ myapp/src/screens/main-screen/MainScreen.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import HeaderView from "../../views/header-view/HeaderView";
 import SideView from "../../views/side-view/SideView";
+import BackDropView from "../../views/backdrop-view/BackDropView";
 
 const MainScreen = (props) => {
 	const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
@@ -12,6 +13,9 @@ const MainScreen = (props) => {
 				drawerToggleClickHandler={() => setSideDrawerOpen(!sideDrawerOpen)}
 			/>
 			<SideView onShowSideDrawer={sideDrawerOpen} />
+			{sideDrawerOpen && (
+				<BackDropView handlerClick={() => setSideDrawerOpen(false)} />
+			)}
 			<div style={{ height: "200vh" }}></div>
 		</>
 	);

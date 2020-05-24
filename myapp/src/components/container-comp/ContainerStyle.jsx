7f3e3a3135df0e86a399785e174bba27497db9d4
chattavat.macOs <chattavat.v@gmail.com>
2020-05-24 23:27:29 +0700

    build container component

---
+++ myapp/src/components/container-comp/ContainerStyle.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const WrapperArea = styled.div`
+	width: 100%;
+`;
+
+export const WrapperBody = styled.div`
+	width: 80%;
+	margin: auto;
+
+	@media (max-width: 700px) {
+		width: 100%;
+		padding: 0 1rem;
+	}
+`;

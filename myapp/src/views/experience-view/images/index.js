eb703371c119883777e653c8688f3c5f3bcdbc43
chattavat.macOs <chattavat.v@gmail.com>
2020-06-07 13:53:08 +0700

    initial hardware experience

---
+++ myapp/src/views/experience-view/images/index.js
@@ -0,0 +1,17 @@
+import deliveryRobotBanner from "./delivery-robot-banner.png";
+import autoSorterBanner from "./auto-sorter-banner.png";
+import bookdropBanner from "./book-drop-banner.png";
+import accessControlBanner from "./access-control-banner.png";
+import flapgateBanner from "./flap-gate-banner.png";
+import swingGateBanner from "./swing-gate-banner.png";
+import staffStationBanner from "./staff-station-banner.png";
+import miniStaffStationBanner from "./mini-staff-station-banner.png";
+
+export const imgDeliveryRobotBanner = deliveryRobotBanner;
+export const imgAutoSorterBanner = autoSorterBanner;
+export const imgBookdopBanner = bookdropBanner;
+export const imgAccessControlBanner = accessControlBanner;
+export const imgFlapgateBanner = flapgateBanner;
+export const imgSwingGateBanner = swingGateBanner;
+export const imgStaffStationBanner = staffStationBanner;
+export const imgMiniStaffStation = miniStaffStationBanner;
---
+++ myapp/src/views/experience-view/utils.js
@@ -0,0 +1,145 @@
+import * as Img from "./images";
+
+export const listExperience = [
+	{
+		name: "DELIVERY ROBOT VERSION 2.0",
+		title:
+			"The Book Drop is an all-in-one self-service library assets return tower model.Its large touch screen and simple interface allow for easy touch interactivity.Scan ID for userauthentication, print out proof of transaction and capture your image. The Book Drop incorporates auto-shutter technology, smart conveyor and built in sensors for exceptional intelligent. The book return system allows the robot to deliver books to the storage bin that has been sorted. Comes from RFID screening system",
+		location: "Sakhonnakorn, Thailand",
+		year: "2018",
+		image: Img.imgDeliveryRobotBanner,
+		category: "hardware",
+		descriptions: [
+			"Research and development of book return systems and sorting into books.",
+			"Develop electronic circuits of all systems.",
+			"Develop functional architecture of the entire system.",
+			"Develop software to control all system functions.",
+			"Installation of hardware and mechanics.",
+			"Develop API used functional communication between Hardware and software frontend.",
+			"Design database system using NoSQL (MongoDB).",
+		],
+	},
+	{
+		name: "AUTO SORTER VERSION.1.0",
+		title:
+			"The Book Drop is an all-in-one self-service library assets return tower model.Its large touch screen and simple interface allow for easy touch interactivity.Scan ID for userauthentication, print out proof of transaction and capture your image. The Book Drop incorporates auto-shutter technology, smart conveyor and built in sensors for exceptional intelligent. The Book Drop T1, experience the newer and smarter way.",
+		location: "Nakron Ratchasima",
+		year: "2016",
+		image: Img.imgAutoSorterBanner,
+		category: "hardware",
+		descriptions: [
+			"Research and development of book return systems and sorting into books.",
+			"Electronic circuit design.",
+			"System architecture design.",
+			"Develop software to control all system functions.",
+			"Installation of hardware and mechanics.",
+			"Customize the book separation system to match the system of the university library.",
+		],
+	},
+	{
+		name: "BOOK DROP VERSION.1.0",
+		title:
+			"The Book Drop is an all-in-one self-service library assets return tower model.Its large touch screen and simple interface allow for easy touch interactivity.Scan ID for userauthentication, print out proof of transaction and capture your image. The Book Drop incorporates auto-shutter technology, smart conveyor and built in sensors for exceptional intelligent. The Book Drop T1, experience the newer and smarter way.",
+		location: "Nakhon pathom, Thailand",
+		year: "2016",
+		image: Img.imgBookdopBanner,
+		category: "hardware",
+		descriptions: [
+			"Research and development of book return systems.",
+			"Electronic circuit design.",
+			"System architecture design.",
+			"Develop software to control all system functions.",
+			"Develop Software Management Data log stored on the SD card",
+			"Installation of hardware and mechanics.",
+			"Develop API used functional communication between Hardware and software frontend.",
+		],
+	},
+	{
+		name: "ACCESS CONTROL VERSION.1.0",
+		title:
+			"Automatic access control system design Is designed to determine access rights To relevant internal personnel And prevent misfortune that may be caused by outsiders",
+		location: "Bangkok,Thailand",
+		year: "2018",
+		image: Img.imgAccessControlBanner,
+		category: "hardware",
+		descriptions: [
+			"Electronics circuit design.",
+			"System architecture design.",
+			"Develop software to control all system functions.",
+			"Develop API used functional communication between Hardware and software frontend.",
+		],
+	},
+	{
+		name: "FLAP GATE VERSION.1.0",
+		title:
+			"The flip gate is more than just a gate. With built in sensors and identification technology, the flip gate accurately identify and generate useful reports. Both elegant and safe, the flip gate can be integrated with the security gate or third parties security systems. Flip gate, the next level in accessibility.",
+		location: "Rayong, Thailand",
+		year: "2016",
+		image: Img.imgFlapgateBanner,
+		category: "hardware",
+		descriptions: [
+			"Solve the problem of receiving the user card code from the card reader.",
+			"Electronic circuit design.",
+			"System architecture design.",
+			"Develop the API used to receive",
+			"send data from the card reader between hardware and software frontend.",
+			"Installing hardware accessories and supplemental electronic circuits.",
+		],
+	},
+	{
+		name: "SWING GATE VERSION.1.0",
+		title:
+			"The flip gate is more than just a gate. With built in sensors and identification technology, the flip gate accurately identify and generate useful reports. Both elegant and safe, the flip gate can be integrated with the security gate or third parties security systems. Flip gate, the next level in accessibility.",
+		location: "Bangkok,Thailand",
+		year: "2018",
+		image: Img.imgSwingGateBanner,
+		category: "hardware",
+		descriptions: [
+			"Research and development of automatic entry",
+			"design systems.",
+			"Develop electronic circuits that support Mifare, QR code.",
+			"Develop system architecture.",
+			"Installation of hardware and mechanics.",
+			"Design database system using NoSQL (MongoDB).",
+			"Develop the API used for communication between hardware and software frontend.",
+		],
+	},
+	{
+		name: "STAFF STATION VERSION.2.0",
+		title:
+			"The Staff Station T1 with its simple design, it can be placed atop or hidden under the desktop. Connect to a personal computer and perform tasks with ease. Place tagged library assets in any orientation within the read-zone and process multiple tagged library assets at once.",
+		location: "Bangkok,Thailand",
+		year: "2018",
+		image: Img.imgStaffStationBanner,
+		category: "hardware",
+		descriptions: [
+			"Research and development of a borrow and return system with a new RFID system.",
+			"Develop electronic circuits to be easy and take the least time to produce.",
+			"Develop new system architecture.",
+		],
+	},
+	{
+		name: "MINI STAFF VERSION.2.0",
+		title:
+			"Mini staff station is designed to sit well on the tabletop. This is achieved by its moderate size and weight design. It is also designed to be aesthetically appealing with its sleek surface design and LED illuminations while keeping its functionality. The mini staff station can be easily connected to a computer via USB. It is a simple plug and play contactless card reader solution.",
+		location: "Bangkok,Thailand",
+		year: "2018",
+		image: Img.imgMiniStaffStation,
+		category: "hardware",
+		descriptions: [
+			"Research and development of a borrow and return system with a new RFID system.",
+			"Develop electronic circuits to be easy and take the least time to produce.",
+			"Develop new system architecture",
+		],
+	},
+];
+
+// {
+//   name: "",
+//   title: "",
+//   location: "",
+//   year: "",
+//   image: "",
+//   category: "",
+//   descriptions: []
+// }
